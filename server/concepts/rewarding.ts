4001d31c6292e89e97fa82e49b8b2779ce86cbd8
Ben Sheres <bsheres@mit.edu>
2024-11-30 20:59:38 -0800

    Implemented rewarding concept

--- server/concepts/rewarding.ts
+++ server/concepts/rewarding.ts
@@ -1,5 +1,6 @@
 import { ObjectId } from "mongodb";
 import DocCollection, { BaseDoc } from "../framework/doc";
+import { NotAllowedError, NotFoundError } from "./errors";
 
 export interface RewardDoc extends BaseDoc {
   name: string; // name of reward
@@ -32,17 +33,77 @@ const icons: Set<string> = new Set<string>(["TODO: links/paths to icons here"]);
 export default class RewardingConcept {
   public readonly rewards: DocCollection<RewardDoc>;
 
+  /**
+   * Make an instance of Rewarding
+   * @param collectionName The name of the rewarding collection
+   */
   public constructor(collectionName: string) {
     this.rewards = new DocCollection<RewardDoc>(collectionName);
   }
 
-  // create new reward with name, icon, user id, project id, task id
-  // check if reward already exists for a given task
+  async createReward(name: string, icon: string, user: string, project: ObjectId, task: ObjectId) {
+    await this.assertUniqueNameForUser(name, user);
+    await this.assertTaskHasNoReward(task);
+    const _id = await this.rewards.createOne({ name, icon, user, project, task });
+    return { msg: `Reward successfully created`, reward: _id };
+  }
 
-  // get reward for user
+  /**
+   * Get a user's rewards
+   * @param user Username of the user to check for
+   * @returns A list of the rewards a user has
+   */
+  async getRewardsByUser(user: string) {
+    const rewards = await this.rewards.readMany({ user });
+    return rewards;
+  }
 
-  // get reward for project
+  /**
+   * Get a project's rewards
+   * @param project The objectId of the project to check for
+   * @returns A list of rewards for that project
+   */
+  async getRewardsByProject(project: ObjectId) {
+    const rewards = await this.rewards.readMany({ project });
+    return rewards;
+  }
 
-  // assert task reward already exists --> to be used in create reward fn
-  // i.e., if the task alr has an associated reward, we don't re-reward for the same task
+  /**
+   * Get's the reward assigned to a certain task
+   * @param task The task to check for a reward
+   * @returns The reward associated with the task
+   * @throws NotFoundError if no reward is associated with that task
+   */
+  async getRewardByTask(task: ObjectId) {
+    const reward = this.rewards.readOne({ task });
+    if (!reward) {
+      throw new NotFoundError(`Task ${task} has no reward associated`);
+    }
+    return reward;
+  }
+
+  /**
+   * Check if a user already has a certain reward
+   * @param name The name of the reward to check for
+   * @param user The username of the user to check for
+   * @throws NotAllowedError if the user already has a reward with that name
+   */
+  private async assertUniqueNameForUser(name: string, user: string) {
+    const result = await this.rewards.readOne({ name: name, user: user });
+    if (result) {
+      throw new NotAllowedError(`User: ${user} already has reward ${name}`);
+    }
+  }
+
+  /**
+   * Check if a task already has a reward assigned to it
+   * @param task The task to check
+   * @throws NotAllowedError if task already has a reward
+   */
+  private async assertTaskHasNoReward(task: ObjectId) {
+    const result = await this.rewards.readOne({ task });
+    if (result) {
+      throw new NotAllowedError(`Task ${task} already has a reward!`);
+    }
+  }
 }
